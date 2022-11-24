0a70916566c8cbb7e828d31667a0daaa3c857e9e
Zespre Schmidt <starbops@zespre.com>
2022-11-24 12:38:34 +0800

    Adjust indentation

--- dumper.sh
+++ dumper.sh
@@ -6,10 +6,10 @@ trap "\rm -rf $DUMP_DIR" EXIT
 DUMP_DIR=$(mktemp -p /tmp -d)
 
 kubectl get pods -A -o json | jq -r '.items[] | [.metadata.namespace, .metadata.name] | @tsv' |
-        while IFS=$'\t' read -r namespace name; do
-                kubectl -n "$namespace" get pods "$name" -o yaml > "$DUMP_DIR"/"$namespace"-"$name".yaml
-                kubectl -n "$namespace" logs "$name" --all-containers > "$DUMP_DIR"/"$namespace"-"$name".log
-        done
+  while IFS=$'\t' read -r namespace name; do
+    kubectl -n "$namespace" get pods "$name" -o yaml > "$DUMP_DIR"/"$namespace"-"$name".yaml
+    kubectl -n "$namespace" logs "$name" --all-containers > "$DUMP_DIR"/"$namespace"-"$name".log
+  done
 
 NOW=$(date +%Y%m%d%H%M%S)
 tar -zcvf dump-"$NOW".tar.gz "$DUMP_DIR"
