0c725e70bcd9cd016dcbe254678a3f17a09bcd5e
Zespre Schmidt <starbops@zespre.com>
2022-11-24 12:14:14 +0800

    Initial commit

---
+++ dumper.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -x
+
+DUMP_DIR=/tmp/dump
+
+mkdir -p "$DUMP_DIR"
+
+kubectl get pods -A -o json | jq -r '.items[] | [.metadata.namespace, .metadata.name] | @tsv' |
+        while IFS=$'\t' read -r namespace name; do
+                kubectl -n "$namespace" get pods "$name" -o yaml > "$DUMP_DIR"/"$namespace"-"$name".yaml
+                kubectl -n "$namespace" logs "$name" --all-containers > "$DUMP_DIR"/"$namespace"-"$name".log
+        done
